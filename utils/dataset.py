b73753f562d87275d1ade6d98ef6ff1c6863f7e9
themdanas <mdanasakbari@gmail.com~>
2026-07-06 10:46:49 +0530

    debug

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,7 +5,7 @@ from utils.formatting import formatting_prompts_func
 def load_training_dataset(tokenizer):
     dataset = load_dataset(
         "yahma/alpaca-cleaned",
-        split="train",
+        split="train[:1000]",
     )
 
     dataset = dataset.map(formatting_prompts_func, 
